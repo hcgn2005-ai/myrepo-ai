7106d3e053b53a3330617c823174dc56d12a834c
hcgn2005-ai <hcgn2005@gmail.com>
2025-11-30 15:57:39 +0530

    Remove plugin management from settings.gradle.kts
    Removed plugin management configuration from settings.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,2 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-        maven { url = uri("https://recloudstream.github.io/gradle/") }
-    }
-}
-
 rootProject.name = "myrepo-ai"
 include(":Anikai")
