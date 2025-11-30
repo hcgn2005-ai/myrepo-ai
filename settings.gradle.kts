b47fc8f7496c014c491f5df801bfaa8a097fb6cc
hcgn2005-ai <hcgn2005@gmail.com>
2025-11-30 14:06:59 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,9 @@
 pluginManagement {
     repositories {
+        maven { url = uri("https://repo.recloudstream.org/releases") }
         gradlePluginPortal()
         google()
         mavenCentral()
-        maven("https://jitpack.io")
-        maven("https://github.com/recloudstream/cloudstream/raw/master") // CloudStream gradle plugin repo
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
     }
 }
 
