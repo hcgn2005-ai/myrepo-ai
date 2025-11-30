fa04b7532a87f38822ac59373d4d20740597a0da
hcgn2005-ai <hcgn2005@gmail.com>
2025-11-30 12:36:11 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("com.android.library") version "8.0.2" apply false
     kotlin("android") version "1.9.0" apply false
     id("com.github.recloudstream.cloudstream") version "0.1.1" apply false
 }
