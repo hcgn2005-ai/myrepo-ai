4c4ff271af18f166abdbd4b33585e90376ba5387
hcgn2005-ai <hcgn2005@gmail.com>
2025-11-30 12:41:30 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,21 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://github.com/recloudstream/cloudstream/raw/master") // CloudStream gradle plugin repo
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
 rootProject.name = "myrepo-ai"
 include(":Anikai")
