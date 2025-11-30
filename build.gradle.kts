b1476c869573e78af927dc64919ecdab7bd20b72
hcgn2005-ai <hcgn2005@gmail.com>
2025-11-30 14:13:26 +0530

    Remove allprojects repositories configuration
    Removed allprojects block and its repositories configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,3 @@ plugins {
     kotlin("android") version "1.9.0" apply false
     id("com.github.recloudstream.cloudstream") version "0.1.1" apply false
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
